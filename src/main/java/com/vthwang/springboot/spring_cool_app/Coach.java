d43af1da9129559aec3ce49911926b88537a2104
vthwang <vincent@fishboneapps.com>
2025-03-31 13:12:23 -0700

    feat: add dependecy injection
    Signed-off-by: vthwang <vincent@fishboneapps.com>

---
+++ src/main/java/com/vthwang/springboot/spring_cool_app/Coach.java
@@ -0,0 +1,5 @@
+package com.vthwang.springboot.spring_cool_app;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ src/main/java/com/vthwang/springboot/spring_cool_app/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.vthwang.springboot.spring_cool_app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
