839b18beb3b1922158e0055c2317f8e0521a9667
vthwang <vincent@fishboneapps.com>
2025-03-30 23:58:36 -0700

    feat: first commit
    Signed-off-by: vthwang <vincent@fishboneapps.com>

---
+++ src/main/java/com/vthwang/springboot/spring_cool_app/SpringCoolAppApplication.java
@@ -0,0 +1,13 @@
+package com.vthwang.springboot.spring_cool_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoolAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vthwang/springboot/spring_cool_app/SpringCoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vthwang.springboot.spring_cool_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
