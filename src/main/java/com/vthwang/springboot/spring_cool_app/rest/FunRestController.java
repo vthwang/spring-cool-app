4b87ae9072af7dc3a42a175f4f3f198a3b60d02d
vthwang <vincent@fishboneapps.com>
2025-03-31 12:39:38 -0700

    feat: setup properties
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/main/java/com/vthwang/springboot/spring_cool_app/rest/FunRestController.java
+++ src/main/java/com/vthwang/springboot/spring_cool_app/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.vthwang.springboot.spring_cool_app.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team: " + teamName;
+    }
 
     // expose "/" that returns "Hello World"
     @GetMapping("/")
