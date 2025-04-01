a6464818069ac393d920ebb03c77cd4452e1398a
vthwang <vincent@fishboneapps.com>
2025-04-01 16:10:17 -0700

    feat: add @Primary
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/main/java/com/vthwang/springboot/spring_cool_app/common/CricketCoach.java
+++ src/main/java/com/vthwang/springboot/spring_cool_app/common/CricketCoach.java
@@ -1,8 +1,10 @@
 package com.vthwang.springboot.spring_cool_app.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class CricketCoach implements Coach {
 
     @Override
--- src/main/java/com/vthwang/springboot/spring_cool_app/rest/DemoController.java
+++ src/main/java/com/vthwang/springboot/spring_cool_app/rest/DemoController.java
@@ -1,7 +1,6 @@
 package com.vthwang.springboot.spring_cool_app.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +12,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(@Qualifier("baseballCoach") Coach theCoach) {
+    public DemoController(Coach theCoach) {
         myCoach = theCoach;
     }
 
