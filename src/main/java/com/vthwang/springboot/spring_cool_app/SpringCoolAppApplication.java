209a66e738f20ffce018981f628c006876305a34
vthwang <vincent@fishboneapps.com>
2025-04-01 17:06:09 -0700

    feat: setup MySQL
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/main/java/com/vthwang/springboot/spring_cool_app/SpringCoolAppApplication.java
+++ src/main/java/com/vthwang/springboot/spring_cool_app/SpringCoolAppApplication.java
@@ -1,7 +1,9 @@
 package com.vthwang.springboot.spring_cool_app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCoolAppApplication {
@@ -10,4 +12,10 @@ public class SpringCoolAppApplication {
 		SpringApplication.run(SpringCoolAppApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
 }
