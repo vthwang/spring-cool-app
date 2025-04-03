b29ac75f767ec172c73f2ba12cefe93afc29a725
vthwang <vincent@fishboneapps.com>
2025-04-03 13:54:24 -0700

    fix: remove imports
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/main/java/com/vthwang/springboot/spring_cool_app/rest/StudentRestController.java
+++ src/main/java/com/vthwang/springboot/spring_cool_app/rest/StudentRestController.java
@@ -3,9 +3,6 @@ package com.vthwang.springboot.spring_cool_app.rest;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
