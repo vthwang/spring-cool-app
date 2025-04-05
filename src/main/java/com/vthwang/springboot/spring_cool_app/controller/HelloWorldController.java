7154f81e002c3ebbcd049acb22685d84eb2d2464
vthwang <vincent@fishboneapps.com>
2025-04-05 14:13:54 -0700

    feat: using GetMapping and PostMapping
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/main/java/com/vthwang/springboot/spring_cool_app/controller/HelloWorldController.java
+++ src/main/java/com/vthwang/springboot/spring_cool_app/controller/HelloWorldController.java
@@ -1,14 +1,17 @@
 package com.vthwang.springboot.spring_cool_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @Controller
 public class HelloWorldController {
     
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm() {
         return "helloworld-form";
     }
@@ -27,7 +30,7 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    @RequestMapping("/processFormVersionThree")
+    @PostMapping("/processFormVersionThree")
     public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
         theName = theName.toUpperCase();
         String result = "Hey My Friend from v3! " + theName;
