1d7df8ef9eed19027d58fc4cdcc8f77b53325ac3
vthwang <vincent@fishboneapps.com>
2025-04-05 14:09:52 -0700

    feat: Spring MVC Binding Request Params
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/main/java/com/vthwang/springboot/spring_cool_app/controller/HelloWorldController.java
+++ src/main/java/com/vthwang/springboot/spring_cool_app/controller/HelloWorldController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import jakarta.servlet.http.HttpServletRequest;
-
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloWorldController {
     
@@ -23,6 +23,14 @@ public class HelloWorldController {
         String theName = request.getParameter("studentName");
         theName = theName.toUpperCase();
         String result = "Yo! " + theName;
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+        theName = theName.toUpperCase();
+        String result = "Hey My Friend from v3! " + theName;
         model.addAttribute("message", result);
         return "helloworld";
     }
