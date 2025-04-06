75051f3d01db69bba5d79074d2ee97e40a99378d
vthwang <vincent@fishboneapps.com>
2025-04-05 20:03:57 -0700

    feat: number validation
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/main/java/com/vthwang/springboot/spring_cool_app/model/Customer.java
+++ src/main/java/com/vthwang/springboot/spring_cool_app/model/Customer.java
@@ -2,15 +2,21 @@ package com.vthwang.springboot.spring_cool_app.model;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
 
 public class Customer {
     private String firstName;
-    
+
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName = "";
 
-    public String getFirstName() {  
+    @Min(value = 0, message = "must be greater than or equal to 0")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private int freePasses;
+
+    public String getFirstName() {
         return firstName;
     }
 
@@ -24,5 +30,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
